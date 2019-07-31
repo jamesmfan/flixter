4f8d5749f7f21413b99c70a01d555283d3bf7bf1
James Fanizza <jamesfanizza@gmail.com>
2019-07-31 18:33:15 +0000

    Revert to 3101dc8fb529e1301ff215dca2f95aa73887f1ff

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -3,9 +3,9 @@ class Instructor::LessonsController < ApplicationController
   before_action :require_authorized_for_current_section, only: [:new, :create]
   before_action :require_authorized_for_current_lesson, only: [:update]
 
-  #def new
-    #@lesson = Lesson.new
-  #end
+  def new
+    @lesson = Lesson.new
+  end
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
